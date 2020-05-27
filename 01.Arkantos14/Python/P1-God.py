be2af01005be4f5b56858695cb734b4b21f52742
AgustinVal <a.valenzuela.1@ug.uchile.cl>
2020-05-27 16:14:28 -0400

    Resuelve P1-God

---
+++ 01.Arkantos14/Python/P1-God.py
@@ -0,0 +1,44 @@
+'''
+Encontrar el area minima de un terreno en el que se puede
+colocar dos casas rectangulares identicas de area a x b 
+Estas pueden estar rotadas (una o ambas), tambien tocar sus
+lados, pero como cualquier par de casas, no pueden solaparse
+'''
+import numpy as np
+
+#Datos
+t = int(input()) #Numero de llamadas
+lista_resul = np.zeros(t, dtype=int) #Vector de 0's de tamaño t, donde se pondran los resultados de las areas
+
+
+#Se evalua cada par de datos, dejando el area obtenida guardada en el vector de zeros
+for i in range(t):
+    a, b = input().split() #Para introducir mas de un valor, separados por un espacio
+
+    #Se introducen constantes convenientes
+    lado_chico = min(int(a), int(b))
+    lado_grande = max(int(a), int(b))
+
+    if(2*lado_chico <= lado_grande):
+        lista_resul[i] = lado_grande**2
+
+    else: #(2*lado_chico > lado_grande): 
+        lista_resul[i] = (2 * lado_chico)**2
+
+#Se imprimen los resultados
+for j in range(t):
+    print(lista_resul[j])
+
+
+
+
+    
+
+        
+
+
+
+
+
+
+    
